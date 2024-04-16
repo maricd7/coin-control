d9566f5ebb5ad8eb39181892acc8f859ca337fd1
unknown <dejanmaric259@gmail.com>
2024-04-16 22:52:49 +0200

    refresh

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import {SignUp, Login} from '../Controllers/AuthController.js';
-import Transaction from '../Models/Transaction.js';
 
 const router = express.Router();
 
 router.post('/signup', SignUp);
 router.post('/login', Login)
-router.post('/transactions',Transaction)
 export default router;
