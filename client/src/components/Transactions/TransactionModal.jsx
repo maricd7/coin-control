fd23260666c6112d5bba6188ccaac24a60208342
unknown <dejanmaric259@gmail.com>
2024-04-23 14:56:42 +0200

    fixing hardcoded select

--- client/src/components/Transactions/TransactionModal.jsx
+++ client/src/components/Transactions/TransactionModal.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef,useState } from 'react'
 import Input from '../common/Input/Input'
 import CtaButton from '../common/Buttons/Cta/CtaButton'
 import { Icon } from '@iconify/react/dist/iconify.js'
@@ -9,14 +9,14 @@ export default function TransactionModal({setTransactionModal,username}) {
 
   const nameRef = useRef();
   const amountRef = useRef(); 
-  
+  const [selectedOption, setSelectedOption] = useState('Income'); 
 
   const handleSumit = async(e)=>{
     e.preventDefault();
    try{
     axios.post('http://localhost:5050/transactions' , {
       name: nameRef.current?.value,
-      transactionType:"expanse",
+      transactionType:selectedOption,
       amount: amountRef.current?.value,
       username:username,
     })
@@ -24,6 +24,7 @@ export default function TransactionModal({setTransactionModal,username}) {
     console.log('error submitting transaction')
    }
   }
+
   return (
     <div className='bg-zinc-800 p-8 rounded-lg shadow-lg w-full max-w-fit absolute -translate-y-1/2 -translate-x-1/2 top-1/2 left-1/2 '>
         <div className='flex w-full justify-between items-center gap-4'>
@@ -33,7 +34,7 @@ export default function TransactionModal({setTransactionModal,username}) {
         <form className='w-full flex flex-col gap-4' onSubmit={(e)=>handleSumit(e)}>
             <Input placeholder='Name of the transaction' type='text' reference={nameRef}/>
             <Input placeholder='Enter value' type='number' reference={amountRef}/>
-            <select className='w-full'>
+            <select className='w-full'  value={selectedOption} onChange={(e) => setSelectedOption(e.target.value)}>
                 <option>Income</option>
                 <option>Expanse</option>
             </select>
