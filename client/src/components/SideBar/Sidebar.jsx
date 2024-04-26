f26b1a756721074f3bfcaa18c826a32a3b9a980b
unknown <dejanmaric259@gmail.com>
2024-04-26 07:08:56 +0200

    adding sidebar

---
+++ client/src/components/SideBar/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div>
+        <li>Profile</li>
+        <li>Profile</li>
+        <li>Profile</li>
+        <li>Profile</li>
+    </div>
+  )
+}
+
+export default Sidebar
