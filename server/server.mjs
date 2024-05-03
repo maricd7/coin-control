d2acf77b51b6c44458c3ecb791f0b2648d3ff760
unknown <dejanmaric259@gmail.com>
2024-05-03 23:29:34 +0200

    vercel cors bug

--- server/server.mjs
+++ server/server.mjs
@@ -10,15 +10,15 @@ import Transaction from "./Models/Transaction.mjs";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
+
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://coin-control-wine.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
-
-
-app.use(express.json());
 
 
 
