08ae58d0ba6bdc9ad09074108af228b6c77dde3e
unknown <dejanmaric259@gmail.com>
2024-05-03 23:35:40 +0200

    cors config

--- server/server.mjs
+++ server/server.mjs
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://coin-control-wine.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
